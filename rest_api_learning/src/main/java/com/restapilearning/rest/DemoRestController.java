7e286cde7c6fddd79e45fdf9adf3c81a2be372da
AkashSenshi <158811195+AkashSenshi@users.noreply.github.com>
2024-02-10 03:06:51 +0530

    Add files via upload

---
+++ rest_api_learning/src/main/java/com/restapilearning/rest/DemoRestController.java
@@ -0,0 +1,16 @@
+package com.restapilearning.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+
+	@GetMapping("/home")
+	public String sayHello() {
+		return "Hello Akash";
+	}
+	
+}
